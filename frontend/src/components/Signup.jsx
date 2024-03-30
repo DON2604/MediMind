5110c26419c2c90a35e3711d2df5e09e3cfa0cfe
Raj Bhattacharyya <rajbhattacharyya18110@gmail.com>
2024-03-30 23:26:30 +0530

    small ui change

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,16 +1,16 @@
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import logo from "../assets/logo.jpeg";
 import DOCImage from "../assets/login.jpg";
 
 const Signup = () => {
   const [formData, setFormData] = useState({
-    username: '',
-    age: '',
-    email: '',
-    password: '',
-    gender: ''
+    username: "",
+    age: "",
+    email: "",
+    password: "",
+    gender: "",
   });
 
   const navigate = useNavigate();
@@ -22,32 +22,53 @@ const Signup = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post('/api/users/register', formData)
-      .then(result => {
+    axios
+      .post("/api/users/register", formData)
+      .then((result) => {
         console.log(result);
-        navigate('/login');
+        navigate("/login");
       })
-      .catch(err => console.log(err))
+      .catch((err) => console.log(err));
   };
 
   return (
     <>
-    <div className="flex justify-center items-center h-screen gap-3 bg-cover" style={{ backgroundImage: `url(${DOCImage})`, filter: 'blur(50px)' }}>
+      <div
+        className="flex justify-center items-center h-screen gap-3"
+        style={{ position: "relative" }}
+      >
+        <img
+          src={DOCImage}
+          alt="DOC"
+          className="h-screen rounded-xl shadow-green-500 shadow-md"
+          style={{
+            filter: "blur(5px)",
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            zIndex: -1,
+          }}
+        />
         <img
           src={DOCImage}
           alt="DOC"
           className="h-2/3 rounded-xl shadow-green-500 shadow-md"
         />
+
         <div className="bg-lblack h-2/3 w-1/4 rounded-xl shadow-green-500 shadow-md ">
           <div className="flex items-center ml-2 mt-3">
             <img src={logo} alt="Logo" className="w-12 h-12 rounded-full" />
             <Link to="/" className="ml-2 font-semibold">
-              MediMind-AI &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <strong>SignUp</strong>
+              MediMind-AI &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+              &nbsp; &nbsp; &nbsp; &nbsp; <strong>SignUp</strong>
             </Link>
           </div>
           <form onSubmit={handleSubmit} className="px-4 py-2">
             <div className="mt-4">
-              <label htmlFor="username" className="block text-green-300">Username:</label>
+              <label htmlFor="username" className="block text-green-300">
+                Username:
+              </label>
               <input
                 type="text"
                 id="username"
@@ -59,7 +80,9 @@ const Signup = () => {
             </div>
             <div className="mt-4 flex justify-between">
               <div className="w-1/2 pr-2">
-                <label htmlFor="age" className="block text-green-300">Age:</label>
+                <label htmlFor="age" className="block text-green-300">
+                  Age:
+                </label>
                 <input
                   type="text"
                   id="age"
@@ -70,7 +93,9 @@ const Signup = () => {
                 />
               </div>
               <div className="w-1/2 pl-2">
-                <label htmlFor="gender" className="block text-green-300">Gender:</label>
+                <label htmlFor="gender" className="block text-green-300">
+                  Gender:
+                </label>
                 <input
                   type="text"
                   id="gender"
@@ -82,7 +107,9 @@ const Signup = () => {
               </div>
             </div>
             <div className="mt-4">
-              <label htmlFor="email" className="block text-green-300">Email:</label>
+              <label htmlFor="email" className="block text-green-300">
+                Email:
+              </label>
               <input
                 type="email"
                 id="email"
@@ -93,7 +120,9 @@ const Signup = () => {
               />
             </div>
             <div className="mt-4">
-              <label htmlFor="password" className="block text-green-300">Password:</label>
+              <label htmlFor="password" className="block text-green-300">
+                Password:
+              </label>
               <input
                 type="password"
                 id="password"
@@ -104,13 +133,18 @@ const Signup = () => {
               />
             </div>
             <div className="mt-4 text-center">
-              <button type="submit" className="bg-green-600 text-white px-4 py-2 rounded-md hover:bg-green-800 mt-3">SignUp</button>
+              <button
+                type="submit"
+                className="bg-green-600 text-white px-4 py-2 rounded-md hover:bg-green-800 mt-3"
+              >
+                SignUp
+              </button>
             </div>
           </form>
         </div>
       </div>
     </>
   );
-}
+};
 
 export default Signup;
